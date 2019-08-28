3a4ca6ee34ef1f6dbebb67970282d529452e3d1c
mahmoud.adel <Mohamed.adel@approcks.com>
2019-08-28 16:12:58 +0200

    jwt middleware and exception

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -7,8 +7,18 @@ use App\Model\Question;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class QuestionController extends Controller
 {
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['index' , 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
